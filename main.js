9ef731eb8ecf74f209d21ea94231182a464396e4
Daniil-Kudlach <daniil.kudlach2018@gmail.com>
2020-03-18 19:27:17 +0200

    add array sort

--- main.js
+++ main.js
@@ -6,9 +6,7 @@
             return el.content.$t.replace(/\w+\:\s*/g, '').split(',')
         }))
         .then(data => {
-            data.sort((a,b)=>{
-                return a[0] > b[0];
-            })
+            data = mergeSort(data);
             data.forEach(el => {
                let src = `https://drive.google.com/uc?id=${el[1]}&export=download`;
                src = src.replace(/\s/g, '');
@@ -19,3 +17,36 @@
                 document.querySelector('.container').innerHTML += card;
             })
         })
+
+        function mergeSort(unsortedArray) {
+            //условие выхода из рекурсии
+            //если переданный массив имеет менее двух элементов - нечего сортировать
+            if (unsortedArray.length < 2) {
+                return unsortedArray;
+            }
+            //находим центр при помощи побитовой операции сдвига на 1 бит вправо
+            //аналог деления на два и округления - только эффективнее
+            const middle = unsortedArray.length >> 1;
+            const left = unsortedArray.slice(0, middle);
+            const right = unsortedArray.slice(middle);
+            //сама сортировка
+            function merge(left, right) {
+                const resultArray = [];
+                let leftIndex = 0;
+                let rightIndex = 0;
+                while (leftIndex < left.length && rightIndex < right.length) {
+                    if (left[leftIndex] < right[rightIndex]) {
+                        resultArray.push(left[leftIndex]);
+                        leftIndex++;
+                    } else {
+                        resultArray.push(right[rightIndex]);
+                        rightIndex++;
+                    }
+                }
+                return resultArray
+                    .concat(left.slice(leftIndex))
+        
+                    .concat(right.slice(rightIndex));
+            }
+            return merge(mergeSort(left), mergeSort(right));
+        }
