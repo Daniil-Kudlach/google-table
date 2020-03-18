d92f02b99e5ea994e72875bed7e8babe51a67bc2
Daniil-Kudlach <daniil.kudlach2018@gmail.com>
2020-03-18 14:21:49 +0200

    new way to fetch img

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-let src = '',
-img;
-fetch('https://spreadsheets.google.com/feeds/cells/1BuePN0GHsl2ig48EYF2Z9Amx6aA94tE9lYTTy-tg4dY/1/public/full?alt=json')
-.then(response=>response.json())
-.then(data=>src = data.feed.entry)
-.then(arr=>arr.map(el => {
-    let i = el.content.$t.indexOf('id=') + 3;
-        return el.content.$t.substring(i);
-}).forEach(el => {
-    let img=new Image();
-    img.src = `https://drive.google.com/uc?id=${el}&export=download`;
-    document.body.prepend(img);    
-}))
+let i =0;
+while(i<1000){
+    fetch('https://spreadsheets.google.com/feeds/list/1BuePN0GHsl2ig48EYF2Z9Amx6aA94tE9lYTTy-tg4dY/1/public/full?alt=json')
+    .then(response=>response.json())
+    .then(({feed})=>[...feed.entry].map(el=>{
+        return el.content.$t.replace(/\w+\:\s*/g, '').replace(/\s/g, '').split(',')
+    }))
+    .then(data=>{
+        data.forEach(el=>{
+            let card = `<div class='card'>
+            <h3>Card</h3>
+                <img src="https://drive.google.com/uc?id=${el[1]}&export=download"></img>
+                <span>${el[0]}</span></div>`
+            document.querySelector('.container').innerHTML += card;
+        })
+    })
+    i++;
+}
